6650fe279097726d498f6315dcb92cb9fc944098
Masayuki Tanaka <mastnk@gmail.com>
2023-04-25 11:19:27 +0900

    multiclass

--- transfer/multiclass.py
+++ transfer/multiclass.py
@@ -17,7 +17,6 @@ cnn = CNN('vgg11')
 
 dir_train = "data/train/"
 dir_test = "data/test/"
-
 labels = ["0", "1", "2", "3"]
 
 def dataset(dir):
@@ -43,9 +42,10 @@ def dataset(dir):
 
     return X, Y
 
-def softmax_proba( proba, alpha=20 ):
-    proba = proba[:,:,1].transpose(1,0)
-    ex = np.exp(proba*alpha)
+def softmax_proba( proba, proba_max=0.99999 ):
+    proba = np.clip( proba[:,:,1].transpose(1,0), 0, proba_max )
+    logit = np.log( proba ) - np.log( 1-proba )
+    ex = np.exp(logit)
     s = ex.sum(axis=1,keepdims=True)
     p = ex/s
     return p
@@ -65,6 +65,11 @@ X_test, Y_test = dataset(dir_test)
 clf = MultiOutputClassifier( LogisticRegression() ).fit(X_train, Y_train)
 Y_pred = prediction( clf, X_test )
 
-print( Y_pred )
-print( acc( Y_test, Y_pred ) )
+Y_ind = np.argmax(Y_pred,axis=1)
+Y_true = np.argmax(Y_test,axis=1)
+for i in range(Y_test.shape[0]):
+    cm = "OK" if Y_ind[i] == Y_true[i] else "NG"
+    print( "{:.4f}, {:d}, {:d}, {:s}".format( Y_pred[i,Y_ind[i]], Y_ind[i], Y_true[i], cm ) )
+print()
+print( "Acc: ", acc( Y_test, Y_pred ) )
 
